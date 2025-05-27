f06c1de458d50cc605c787eed598e0715a9a4770
Alexeigithubproject <alexeiprsk@gmail.com>
2025-05-27 05:28:15 +0300

    betslip file fix

--- src/components/Betslip/BetSlip.jsx
+++ src/components/Betslip/BetSlip.jsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { useState } from 'react';
-import BetSlipButton from './BetSlipButton';
+import BetSlipButton from './BetslipButton';
 import BetslipView from './BetSlipView';
 import BetslipOpenBets from './BetslipOpenBets';
 
