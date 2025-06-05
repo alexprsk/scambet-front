adb255586726af29249e12c56af8cf213e32300a
alexei <alexeiprsk@gmail.com>
2025-06-05 13:24:30 +0300

    back to local

--- src/hooks/useFetchMarkets.js
+++ src/hooks/useFetchMarkets.js
@@ -7,7 +7,7 @@ export const useFetchMarkets = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch('https://scambet.org/sportsbook/odds');
+        const res = await fetch('https://localhost:8000/sportsbook/odds');
         const json = await res.json();
 
         const transformed = (json.events[0] || [])
