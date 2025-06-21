7479f56e075bb37ce18c9dc5b27ea6a7aca2f863
Alexeigithubproject <alexeiprsk@gmail.com>
2025-06-21 16:57:34 +0300

    fixed images'

--- src/constants/images.jsx
+++ src/constants/images.jsx
@@ -2,8 +2,9 @@ import MingPic from '../assets/ming.png'
 import Ouch from '../assets/ouch.jpg'
 import FishAround from '../assets/fisharound.png'
 import SurfDog from '../assets/surfdog.png'
-import Kefalitos from '../assets/Kefalitos.jpg'
-import Kefalitos2 from '../assets/Kefalitos2.jpg'
+import Kefalitos from '../assets/Kefalitos.png'
+import Kefalitos2 from '../assets/Kefalitos2.png'
+
 
 
 export const IMAGES = [
